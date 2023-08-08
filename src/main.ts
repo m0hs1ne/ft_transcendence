fc17f5fbf220d256c34ae2d6cc2e8ee905612d3f
m0hs1ne <mel-hada@student.1337.ma>
2023-08-08 17:07:49 +0100

    fix docker compose

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as passport from 'passport'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+    origin: '*',
     credentials: true,
   });
   app.setGlobalPrefix('api');
