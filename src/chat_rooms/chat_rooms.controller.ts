40f5c88eb4bfb3db6fba173bfb13fedd5dabfc89
MarouaneBenchiekh <marouanebenchiekh@gmail.com>
2023-10-01 16:30:14 +0100

    chat room details api

---
+++ src/chat_rooms/chat_rooms.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { userAuthGuard, verifyToken } from 'src/utils/guard';
+import { ChatRoomsService } from './chat_rooms.service';
+
+@UseGuards(userAuthGuard)
+@Controller('chat-rooms/')
+export class ChatRoomsController {
+    constructor(private readonly chatroomservice: ChatRoomsService) {}
+
+    @Get(':id')
+    async chatroomDetails(@Param('id') id, @Req() req){
+        const payload = verifyToken(req.headers.cookie)
+        const members = await this.chatroomservice.getChatMember(id);
+        const messages = await this.chatroomservice.getMessages('chat', id, payload)
+        let details = {members, messages}
+        return details
+    }
+}
