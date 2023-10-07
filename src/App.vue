6ab42bcaed53b22ed76b08730bc98358693e42c2
Abdeljalil-Bouchfar <bouchfar2000@gmail.com>
2023-10-07 11:41:50 +0100

    Update loading logic

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,13 @@ import Loading1 from './components/Loading/Loading1.vue';
 import Loading2 from './components/Loading/Loading2.vue';
 import axios from 'axios';
 
+
 export default {
+  data() {
+    return {
+      loading: false
+    };
+  },
   mounted() {
     this.checkValidUser()
   },
@@ -20,9 +26,9 @@ export default {
       return allowedPaths.includes(this.$route.path);
     },
     async checkValidUser() {
+      this.loading = true;
       await axios.get('http://localhost:3000/api/auth/success', { withCredentials: true })
         .then(() => {
-          console.log("path is: ", this.$route.path);
           if (this.$route.path.toLowerCase() == '/signin' || this.$route.path.toLowerCase() == '/signup')
             this.$router.push('/');
         })
@@ -30,6 +36,7 @@ export default {
           if (this.$route.path.toLowerCase() != '/signin' && this.$route.path.toLowerCase() != '/signup')
             this.$router.push('/signIn')
         })
+        this.loading = false;
     }
   }
 }
@@ -37,7 +44,8 @@ export default {
 
 <template>
   <main>
-    <Sidebar v-if="isSidebarVisible()" />
-    <RouterView /> 
+    <Loading1 v-if="loading" />
+    <Sidebar v-if="isSidebarVisible() && !loading" />
+    <RouterView v-if="!loading" />
   </main>
 </template>
