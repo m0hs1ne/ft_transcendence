965c932080848d1355f88183bf01ae54a93ee252
MarouaneBenchiekh <marouanebenchiekh@gmail.com>
2023-10-10 16:09:12 +0100

    sultan gha 9ra ylh

--- src/chat_rooms/chat_rooms.controller.ts
+++ src/chat_rooms/chat_rooms.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Delete, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { userAuthGuard, verifyToken } from 'src/utils/guard';
 import { ChatRoomsService } from './chat_rooms.service';
+import { UsersService } from 'src/users/users.service';
 
 @UseGuards(userAuthGuard)
 @Controller('chat-rooms/')
 export class ChatRoomsController {
-    constructor(private readonly chatroomservice: ChatRoomsService) {}
+    constructor(private readonly chatroomservice: ChatRoomsService
+        ,private readonly userservice: UsersService) {}
 
     @Get()
     async myChatRoom(@Req() req){
@@ -14,6 +16,15 @@ export class ChatRoomsController {
         return chatrooms
     }
     
+    @Get('/DM_chatrooms')
+    async all(@Req() req){
+        const payload = verifyToken(req.headers.cookie)
+        const chatrooms = await this.chatroomservice.findMyChatRooms(payload);
+        const friends = await this.userservice.getfriends(payload.sub)
+        return {friends, chatrooms}
+    }
+    
+
     @Get(':id')
     async chatroomDetails(@Param('id') id, @Req() req){
         const payload = verifyToken(req.headers.cookie)
