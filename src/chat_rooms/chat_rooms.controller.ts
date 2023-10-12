6751a6af13954aa6dd66bc6a94d5c4428fd4d924
MarouaneBenchiekh <marouanebenchiekh@gmail.com>
2023-10-12 12:17:07 +0100

    get my role

--- src/chat_rooms/chat_rooms.controller.ts
+++ src/chat_rooms/chat_rooms.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Delete, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { userAuthGuard, verifyToken } from 'src/utils/guard';
 import { ChatRoomsService } from './chat_rooms.service';
 import { UsersService } from 'src/users/users.service';
+import { UserChatService } from 'src/user_chat/user_chat.service';
 
 @UseGuards(userAuthGuard)
 @Controller('chat-rooms/')
 export class ChatRoomsController {
     constructor(private readonly chatroomservice: ChatRoomsService
-        ,private readonly userservice: UsersService) {}
+        ,private readonly userservice: UsersService
+        ,private readonly userchatservice: UserChatService) {}
 
     @Get()
     async myChatRoom(@Req() req){
@@ -22,6 +24,13 @@ export class ChatRoomsController {
         const chatrooms = await this.chatroomservice.findMyChatRooms(payload);
         const friends = await this.userservice.getfriends(payload.sub)
         return friends.concat(chatrooms)
+    }
+
+    @Get('myrole/:id')
+    async myrole(@Param('id') id, @Req() req)
+    {
+        const payload = verifyToken(req.headers.cookie)
+        const role = await this.userchatservice.myrole(id, payload.sub)
     }
     
     @Get(':id')
--- src/user_chat/user_chat.service.ts
+++ src/user_chat/user_chat.service.ts
@@ -1,26 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserChatDto } from './dto/create-user_chat.dto';
 import { UpdateUserChatDto } from './dto/update-user_chat.dto';
+import { UserChat } from './entities/user_chat.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserChatService {
-  create(createUserChatDto: CreateUserChatDto) {
-    return 'This action adds a new userChat';
-  }
+  constructor(
+    @InjectRepository(UserChat) private readonly userChatRepository: Repository<UserChat>,
+  ) {}
 
-  findAll() {
-    return `This action returns all userChat`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} userChat`;
-  }
-
-  update(id: number, updateUserChatDto: UpdateUserChatDto) {
-    return `This action updates a #${id} userChat`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} userChat`;
+  async myrole(chatId, id)
+  {
+      const role = this.userChatRepository.findOne({
+        select: ['role'],
+        where: {userId: id, chatRoomId: chatId}
+      })
+      if (!role)
+        throw new NotFoundException();
+      return role
   }
 }
