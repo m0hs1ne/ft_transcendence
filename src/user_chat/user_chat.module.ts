2f3e944940980117b8db9c2a8981a3ee777aa471
MarouaneBenchiekh <marouanebenchiekh@gmail.com>
2023-10-13 10:57:42 +0100

    fix

--- src/user_chat/user_chat.module.ts
+++ src/user_chat/user_chat.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserChatService } from './user_chat.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserChat } from './entities/user_chat.entity';
+import { UsersController } from 'src/users/users.controller';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [UserChatService]
+  imports: [
+    TypeOrmModule.forFeature([UserChat]),
+  ],
+  providers: [UserChatService, JwtService]
 })
 export class UserChatModule {}
