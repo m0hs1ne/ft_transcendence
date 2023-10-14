08f5a520bd5002dd544cbcab9d9a50584152621a
zakaria2ee <zakariaboudaira3@gmail.com>
2023-10-14 11:25:14 +0100

    Modify the game

---
+++ src/game/game.gateway.ts
@@ -0,0 +1,77 @@
+import { Socket } from 'socket.io';
+import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Namespace } from 'socket.io';
+import { Room } from './room';
+import { v4 as uuidv4 } from 'uuid';
+
+
+
+@WebSocketGateway({ namespace: '/game' })
+export class GameGateway {
+  @WebSocketServer() server: Server;
+  // dashboardNamespace: Namespace = this.server.of('/dashboard');
+  private rooms: Map<string, Room> = new Map();
+  private roomsqueu: Socket[] = [];
+
+  handleConnection(client: Socket) {
+    console.log(`A Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket)
+  {
+    console.log(`Client disconnected: ${client.id}`);
+    if(this.roomsqueu.at(0) == client)
+    {
+      this.roomsqueu.pop();
+    }
+  }
+
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(client: Socket, payload: any): void {
+    console.log("JOinRoom");
+    // let room = this.FindAvailableRoom();
+    // console.log(payload);
+    this.roomsqueu.push(client);
+
+    if(this.roomsqueu.length == 2)
+    {
+      let room: Room = new Room();
+      const { v4: uuidv4 } = require('uuid');
+
+      room.RightPlayer = this.roomsqueu.pop();
+      room.LeftPlayer = this.roomsqueu.pop();
+      console.log(room.RightPlayer.id);
+      console.log(room.LeftPlayer.id);
+      room.roomsId = uuidv4();
+      this.rooms[room.roomsId] = room;
+      this.rooms[room.roomsId].Play();
+      console.log("Game Started");
+    }
+    else
+    {
+      console.log("Waiting");
+    }
+  }
+  @SubscribeMessage('PaddleUpdates')
+  HandlePaddlesData(client: any, payload: any): void
+  {
+    if(payload.pos == "Left")
+    {
+      this.rooms[payload.roomId].LeftPlayerPaddle = payload.Paddle;
+      this.rooms[payload.roomId].RightPlayer.emit('OpponentPaddle', {
+        Paddle: payload.Paddle,
+      })
+      // console.log(payload.Paddle, payload.pos, payload.roomId);
+    }
+    else if(payload.pos == "Right")
+    {
+      this.rooms[payload.roomId].RightPlayerPaddle = payload.Paddle;
+      this.rooms[payload.roomId].LeftPlayer.emit('OpponentPaddle', {
+        Paddle: payload.Paddle,
+      })
+    }
+    
+  }
+  
+}
