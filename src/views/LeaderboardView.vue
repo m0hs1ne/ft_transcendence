4b38244d68ca130005f335e99899a1efe5506e64
Abdeljalil-Bouchfar <bouchfar2000@gmail.com>
2023-10-24 16:41:15 +0100

    Leaderboard order fix

--- src/views/LeaderboardView.vue
+++ src/views/LeaderboardView.vue
@@ -15,6 +15,7 @@ export default {
         });
         this.leaderboard = response.data;
         console.log("leaderboard: ", this.leaderboard);
+        this.leaderboard.sort((a, b) => (((b.wins / (b.wins + b.losses)) * 100) - ((a.wins / (a.wins + a.losses)) * 100)));
       } catch (error) {
         console.error("getLeaderBoard:", error);
       }
@@ -27,42 +28,24 @@ export default {
 </script>
 
 <template>
-  <div
-    class="flex flex-col justify-start items-center min-h-screen ml-20 dark:bg-slate-800 p-10"
-  >
+  <div class="flex flex-col justify-start items-center min-h-screen ml-20 dark:bg-slate-800 p-10">
     <h1 class=" font-semibold text-4xl dark:text-white pb-10">
       Leaderboard:
     </h1>
-    <div
-      v-for="(player, index) in this.leaderboard"
-      :key="index"
-      class="flex items-center justify-start w-full max-w-[500px] my-2 px-5 py-3 rounded-2xl custom-box-shadow dark:bg-slate-700 dark:text-white"
-    >
-      <router-link
-        :to="'/users/' + player.id"
-        class="flex items-center justify-between min-w-full"
-      >
+    <div v-for="(player, index) in this.leaderboard" :key="index"
+      class="flex items-center justify-start w-full max-w-[500px] my-2 px-5 py-3 rounded-2xl custom-box-shadow dark:bg-slate-700 dark:text-white">
+      <router-link :to="'/users/' + player.id" class="flex items-center justify-between min-w-full">
         <div class="flex items-center">
           <p class=" font-semibold text-xl">{{ index + 1 }}.</p>
-          <div
-            class="w-14 h-14 md:w-20 md:h-20 bg-gray-300 rounded-full shadow ml-2 mr-4"
-          >
-            <img
-              referrerpolicy="no-referrer"
-              :src="player.avatar"
-              alt="Avatar"
-              class="object-cover rounded-full w-14 h-14 md:w-20 md:h-20"
-            />
+          <div class="w-14 h-14 md:w-20 md:h-20 bg-gray-300 rounded-full shadow ml-2 mr-4">
+            <img referrerpolicy="no-referrer" :src="player.avatar" alt="Avatar"
+              class="object-cover rounded-full w-14 h-14 md:w-20 md:h-20" />
           </div>
-          <p
-            class="w-36 md:w-56 overflow-ellipsis line-clamp-1  font-semibold md:text-xl tracking-wide dark:text-white"
-          >
+          <p class="w-36 md:w-56 overflow-ellipsis line-clamp-1  font-semibold md:text-xl tracking-wide dark:text-white">
             {{ player.username }}
           </p>
         </div>
-        <p
-          class="min-w-fit  font-semibold text-xl md:text-2xl tracking-wide dark:text-white"
-        >
+        <p class="min-w-fit  font-semibold text-xl md:text-2xl tracking-wide dark:text-white">
           {{ parseInt((player.wins / (player.wins + player.losses)) * 100) }}%
         </p>
       </router-link>
