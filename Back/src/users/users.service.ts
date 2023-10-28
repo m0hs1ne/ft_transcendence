85ffbc6877bf99159a288cc304af38dd53fac67e
MarouaneBenchiekh <marouanebenchiekh@gmail.com>
2023-10-28 11:22:03 +0100

    blocked and blockedBy

--- Back/src/users/users.service.ts
+++ Back/src/users/users.service.ts
@@ -289,17 +289,15 @@ export class UsersService {
     const blockedId: FindOptionsWhere<User> = {
       id: id,
     };
-    const friend = await this.userRepository.findOne({
-      relations: ["blockedBy"],
-      where: { id: id },
-    });
+    const friend =await this.userRepository
+    .createQueryBuilder("user")
+    .leftJoinAndSelect("user.blockedBy", "friend")
+    .where("user.id = :id", { id })
+    .getOne();
     if (!friend) {
       throw new NotFoundException();
     }
     const payload = verifyToken(req.headers.cookie);
-    const myId: FindOptionsWhere<User> = {
-      id: payload.sub,
-    };
     const me = await this.userRepository
       .createQueryBuilder("user")
       .leftJoinAndSelect("user.blocked", "friend")
