3f92cb4f461e0ccf5b76576dc84692cd8c0c65a7
abdessamadalami <eloualialamiabdessamad@gmail.com>
2023-10-29 21:06:38 +0100

    merge

--- sri.cpp
+++ sri.cpp
@@ -6,21 +6,14 @@
 int main() {
     std::ifstream file("my.txt"); // Replace with the actual file name
     std::string line;
-    
+    int a = 0;
     while (std::getline(file, line)) {
-        // std::istringstream iss(line);
-        // std::string timestamp, message;
-        // iss >> timestamp;
-        // std::getline(iss, message);
         std::cout << line;
         std::string command = line;
           std::cout << command;
         int result = system(command.c_str());
+        a++;
       
-        // if (result != 0) {
-        //     std::cerr << "Failed to commit: " + message << std::endl;
-        //     return 1;
-        // }
     }
 
     file.close();
